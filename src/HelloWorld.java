5bc269f00b34dfd28c02d778dad32c86d343ca38
htao <htao@thoughtworks.com>
2018-03-13 14:24:34 +0800

    [taohui] handle exception with try catch

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,5 +7,14 @@ public class HelloWorld {
         System.out.println("hello, " + myName);
         Point originOne = new Point(23, 94);
         System.out.println("point: " + originOne.x + ", " + originOne.y);
+
+        try{
+            customeMath math = new customeMath();
+            float result = math.divide(3, 0);
+            System.out.println("divede result: " + result);
+        }
+        catch(ArithmeticException e){
+            System.out.println("divede exception: " + e.getMessage());
+        }
     }
 }
--- src/customeMath.java
+++ src/customeMath.java
@@ -2,4 +2,9 @@ public class customeMath {
     public int add(int i, int y) {
         return i + y;
     }
+
+    public float divide(int a, int b) {
+        float result = a / b;
+        return result;
+    }
 }
